d0093d39ad522d15738eddb5a6d55aac4413375a
YoonSeok Lee <52606813+goodgood619@users.noreply.github.com>
2022-04-15 18:04:06 +0900

    - QuartzScheduler Config 관련 및 간단 구현

---
+++ src/main/java/com/example/wirebarley_coding_test/batch/BatchService.java
@@ -0,0 +1,12 @@
+package com.example.wirebarley_coding_test.batch;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BatchService {
+
+  public void batchCheckExchangeRate() {
+
+  }
+
+}
