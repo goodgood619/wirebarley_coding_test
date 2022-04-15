9c6ef5f436621d04ea4859c653476e37305a559d
YoonSeok Lee <52606813+goodgood619@users.noreply.github.com>
2022-04-15 17:13:13 +0900

    initialize

---
+++ src/main/java/com/example/wirebarley_coding_test/WirebarleyCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.wirebarley_coding_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirebarleyCodingTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WirebarleyCodingTestApplication.class, args);
+  }
+
+}
